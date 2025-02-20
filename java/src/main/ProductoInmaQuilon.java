ddc7e3e0d925ac7cca791964a572a70485d0e88f
Inmaculada Quilón <inmaquilon@gmail.com>
2025-02-20 19:16:21 +0100

    Documentación de código de clase Producto

--- java/src/main/ProductoInmaQuilon.java
+++ java/src/main/ProductoInmaQuilon.java
@@ -1,18 +1,52 @@
 package main;
-
 import java.util.Scanner;
 
+/**
+ * Clase que proporciona operaciones de tipo multiplicación.
+ * @author Inma Quilon
+ */
 public class ProductoInmaQuilon {
 
+    /**
+     * Multiplica dos números reales y devuelve el resultado.
+     * @param real1 es el primer numero a multiplicar
+     * @param real2 es el segundo numero a multiplicar
+     * @return La multiplicación de real1 y real2
+     */
     public static double reales(double real1, double real2) {
         return real1 * real2;
     }
+
+
+    /**
+     * Multiplica dos números enteros y devuelve el resultado.
+     * @param entero1 es el primer numero a multiplicar
+     * @param entero2 es el segundo numero a multiplicar
+     * @return La multiplicación de entero1 y entero2
+     */
     public static int enteros(int entero1, int entero2) {
         return entero1 * entero2;
     }
+
+
+    /**
+     * Multiplica tres números reales y devuelve el resultado.
+     * @param a es el primer numero a multiplicar
+     * @param b es el segundo numero a multiplicar
+     * @param c es el tercer numero a multiplicar
+     * @return La multiplicación de a, b y c
+     */
     public static double tresNumeros(double a, double b, double c) {
         return a*b*c;
     }
+
+
+    /**
+     * Cálcula la potencia de un número real.
+     * @param base es el numero a calcular
+     * @param exp es el exponente al que se va a elevar la base
+     * @return La multiplicación de base por si mismo hasta el numero de veces del exponente
+     */
     public static double potencia(double base, double exp) {
         return Math.pow(base, exp);
     }
